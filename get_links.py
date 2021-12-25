205616ab257669c5ae7d1b547c3f794bda61583c
nguyen-huong <mhuong1703@gmail.com>
2021-12-25 12:50:14 -0800

    Update get_links.py

--- get_links.py
+++ get_links.py
@@ -128,7 +128,7 @@ def aggregate_links(driver):
 
 # 'main' method to iterate through all pages and aggregate URLs
 def getURLs():
-    driver = webdriver.Chrome(executable_path='/usr/local/bin/chromedriver')
+    driver = webdriver.Chrome(executable_path='/opt/homebrew/bin/chromedrive')
     success = login(driver)
     if not success:
         # close the page if it gets stuck at some point - this logic can be improved
